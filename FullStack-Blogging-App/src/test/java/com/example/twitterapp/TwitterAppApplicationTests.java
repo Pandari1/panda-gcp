45631a7e1c51a87c1ec66e2123641290b8c005e2
Pandari1 <166743165+Pandari1@users.noreply.github.com>
2026-03-08 12:59:16 +0530

    Simplify SpringBootTest annotation usagefixed

--- FullStack-Blogging-App/src/test/java/com/example/twitterapp/TwitterAppApplicationTests.java
+++ FullStack-Blogging-App/src/test/java/com/example/twitterapp/TwitterAppApplicationTests.java
@@ -3,7 +3,7 @@ package com.example.twitterapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = TwitterAppApplication.class)
+@SpringBootTest
 class TwitterAppApplicationTests {
 
     @Test
