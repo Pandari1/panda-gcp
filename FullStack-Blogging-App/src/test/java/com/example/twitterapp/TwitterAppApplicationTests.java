632189548c02e129af188613a2caf8fc3c3e2bb2
Pandari1 <166743165+Pandari1@users.noreply.github.com>
2026-03-08 12:51:53 +0530

    ok
    Converted TwitterAppApplicationTests to main application class.

--- FullStack-Blogging-App/src/test/java/com/example/twitterapp/TwitterAppApplicationTests.java
+++ FullStack-Blogging-App/src/test/java/com/example/twitterapp/TwitterAppApplicationTests.java
@@ -1,13 +1,12 @@
 package com.example.twitterapp;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class TwitterAppApplicationTests {
+@SpringBootApplication
+public class TwitterAppApplication {
 
-	@Test
-	void contextLoads() {
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TwitterAppApplication.class, args);
+    }
 }
